c5785bf0ac43ea6c74caa5dc8b4bcb6f54ad566c
Luis Reis <luis.m.reis@gmail.com>
2012-09-13 22:45:04 +0000

    Implement gradients.

---
+++ examples/gradients.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env ./node_modules/openvg/bin/node-pi
+
+/**
+ * Module dependencies.
+ */
+
+var Canvas = require('../lib/canvas')
+  , canvas = new Canvas(320, 320)
+  , ctx = canvas.getContext('2d')
+  , fs = require('fs');
+
+var eu = require('./examples-util');
+
+// Create gradients
+var lingrad = ctx.createLinearGradient(0,0,0,150);
+lingrad.addColorStop(0, '#00ABEB');
+lingrad.addColorStop(0.5, '#fff');
+lingrad.addColorStop(0.5, '#26C000');
+lingrad.addColorStop(1, '#fff');
+
+var lingrad2 = ctx.createLinearGradient(0,50,0,95);
+lingrad2.addColorStop(0.5, '#000');
+lingrad2.addColorStop(1, 'rgba(0,0,0,0)');
+
+// assign gradients to fill and stroke styles
+ctx.fillStyle = lingrad;
+ctx.strokeStyle = lingrad2;
+
+// draw shapes
+ctx.fillRect(10,10,130,130);
+ctx.strokeRect(50,50,50,50);
+
+eu.handleTermination();
+eu.waitForInput();
