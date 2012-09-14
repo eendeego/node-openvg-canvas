48ad674adcdfb46f4bea83c9ac1e18913a1564d2
Luis Reis <luis.m.reis@gmail.com>
2012-09-14 01:59:49 +0000

    Fix named color parsing.

---
+++ examples/spark.js
@@ -0,0 +1,50 @@
+#!/usr/bin/env ./node_modules/openvg/bin/node-pi
+
+/**
+ * Module dependencies.
+ */
+
+var Canvas = require('../lib/canvas')
+  , canvas = new Canvas(40, 15)
+  , ctx = canvas.getContext('2d')
+  , fs = require('fs');
+
+var eu = require('./examples-util');
+
+ctx.clearRect(0,0,canvas.width,canvas.height);
+
+Object.defineProperty(Array.prototype, 'max', {
+  get: function(){
+    var max = 0;
+    for (var i = 0, len = this.length; i < len; ++i) {
+      var n = this[i];
+      if (n > max) max = n;
+    }
+    return max;
+  }
+});
+
+function spark(ctx, data) {
+  var len = data.length
+    , pad = 1
+    , width = ctx.canvas.width
+    , height = ctx.canvas.height
+    , barWidth = width / len
+    , max = data.max;
+  ctx.fillStyle = 'rgba(0,0,255,0.5)';
+  ctx.strokeStyle = 'red';
+  ctx.lineWidth = 1;
+  data.forEach(function(n, i){
+    var x = i * barWidth + pad
+      , y = height * (n / max)
+    ctx.lineTo(x, height - y);
+    ctx.fillRect(x, height, barWidth - pad, -y);
+  });
+  ctx.stroke();
+}
+
+spark(ctx, [1,2,4,5,10,4,2,5,4,3,3,2]);
+
+canvas.vgSwapBuffers()
+eu.handleTermination();
+eu.waitForInput();
