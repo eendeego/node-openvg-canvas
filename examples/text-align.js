ecd35975f51e02bb7dec7440e78b256a7abb0d98
Luis Reis <luis.m.reis@gmail.com>
2012-09-15 05:12:44 +0000

    Review text rendering.

--- examples/text-align.js
+++ examples/text-align.js
@@ -53,41 +53,51 @@ ctx.fillText('top', 640, 600);
 
 ctx.textAlign = 'center';
 ctx.textBaseline = 'alphabetic';
-ctx.font = '160px serif';
+ctx.font = '80px serif';
 
 var m = ctx.measureText("Top");
+var h = m.actualBoundingBoxAscent + m.actualBoundingBoxDescent;
+var hw = m.actualBoundingBoxLeft;
 var y = 300 + (m.actualBoundingBoxAscent + m.actualBoundingBoxDescent) / 2 - m.actualBoundingBoxDescent;
+
+ctx.translate(320, y);
+ctx.scale(2, 2);
 
 ctx.globalAlpha = .5;
 
+ctx.lineWidth = 1/2;
 ctx.beginPath();
-ctx.moveTo(100, 300);
-ctx.lineTo(540, 300);
-ctx.moveTo(100, y);
-ctx.lineTo(540, y);
-ctx.moveTo(320, y - 200);
-ctx.lineTo(320, y + 20);
+ctx.moveTo(-hw * 1.2, -(m.actualBoundingBoxAscent + m.actualBoundingBoxDescent) / 2 + m.actualBoundingBoxDescent);
+ctx.lineTo(+hw * 1.2, -(m.actualBoundingBoxAscent + m.actualBoundingBoxDescent) / 2 + m.actualBoundingBoxDescent);
+ctx.moveTo(-hw * 1.2, 0);
+ctx.lineTo(+hw * 1.2, 0);
+ctx.moveTo(        0, -m.actualBoundingBoxAscent - 0.1 * h);
+ctx.lineTo(        0, m.actualBoundingBoxDescent + 0.1 * h);
 ctx.stroke();
 
+ctx.lineWidth = 2;
 ctx.globalAlpha = 1;
-ctx.fillText('Top', 320, y);
+ctx.fillText('Top', 0, 0);
+ctx.strokeStyle = '#004080';
+ctx.strokeText('Top', 0, 0);
 
+ctx.lineWidth = 1/2;
 ctx.strokeStyle = '#f00';
-ctx.strokeRect(320 + m.actualBoundingBoxLeft,
-               y - m.actualBoundingBoxAscent,
+ctx.strokeRect(m.actualBoundingBoxLeft,
+               -m.actualBoundingBoxAscent,
                m.actualBoundingBoxRight - m.actualBoundingBoxLeft,
                m.actualBoundingBoxAscent + m.actualBoundingBoxDescent);
 
 ctx.strokeStyle = '#00ff80';
 ctx.beginPath();
-ctx.moveTo(100, y + m.emHeightDescent);
-ctx.lineTo(540, y + m.emHeightDescent);
+ctx.moveTo(-hw * 1.2, m.emHeightDescent);
+ctx.lineTo(+hw * 1.2, m.emHeightDescent);
 ctx.stroke();
 
 ctx.strokeStyle = '#0080ff';
 ctx.beginPath();
-ctx.moveTo(100, y - m.emHeightAscent);
-ctx.lineTo(540, y - m.emHeightAscent);
+ctx.moveTo(-hw * 1.2, -m.emHeightAscent);
+ctx.lineTo(+hw * 1.2, -m.emHeightAscent);
 ctx.stroke();
 
 canvas.vgSwapBuffers()
