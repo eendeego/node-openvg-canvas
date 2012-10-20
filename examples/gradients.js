90e36300fece29f05c083654f640314a450b3ad6
Luis Reis <luis.m.reis@gmail.com>
2012-10-21 00:54:15 +0100

    Add gradients examples.

--- examples/gradients.js
+++ examples/gradients.js
@@ -10,7 +10,7 @@ var fs = require('fs');
 var eu = require('./util');
 
 ctx.clearRect(0, 0, canvas.width, canvas.height);
-ctx.scale(4, 4);
+ctx.scale(3, 3);
 
 // Create gradients
 var lingrad = ctx.createLinearGradient(0, 0, 0, 150);
@@ -30,6 +30,26 @@ ctx.strokeStyle = lingrad2;
 // draw shapes
 ctx.fillRect(10, 10, 130, 130);
 ctx.strokeRect(50, 50, 50, 50);
+
+// Default gradient stops
+ctx.fillStyle = '#008000';
+ctx.fillRect(150, 0, 150, 150);
+
+lingrad = ctx.createLinearGradient(150, 0, 300, 150);
+ctx.fillStyle = lingrad;
+ctx.fillRect(160, 10, 130, 130);
+
+// Radial gradients
+ctx.fillStyle = '#a00000';
+ctx.fillRect(0, 150, 150, 150);
+
+lingrad = ctx.createRadialGradient(30, 180,  50, 30, 180, 100);
+lingrad.addColorStop(0, '#00ABEB');
+lingrad.addColorStop(0.5, '#fff');
+lingrad.addColorStop(0.5, '#26C000');
+lingrad.addColorStop(1, '#fff');
+ctx.fillStyle = lingrad;
+ctx.fillRect(10, 160, 130, 130);
 
 canvas.vgSwapBuffers();
 eu.handleTermination();
