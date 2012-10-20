5aa096315dbb8ba9745baf10e5c93d1040222324
Luis Reis <luis.m.reis@gmail.com>
2012-10-20 11:08:29 +0100

    Add example/test for double buffering.

---
+++ examples/doubleBuffering.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node-canvas
+
+/**
+ * Module dependencies.
+ */
+
+var Canvas = require('../lib/canvas');
+var canvas = new Canvas(200, 200);
+var ctx = canvas.getContext('2d');
+
+var eu = require('./examples-util');
+
+ctx.font = canvas.height/4 + 'px sans';
+ctx.textAlign = 'center';
+ctx.textBaseline = 'middle';
+ctx.fillStyle = 'red';
+
+var i = 0;
+(function drawText() {
+  if (i === 0) ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillText(i.toString(),
+               canvas.width * (i % 5) / 5 + canvas.width / 10,
+               canvas.height * Math.floor(i / 5) / 2 + canvas.height / 4);
+  canvas.vgSwapBuffers();
+
+  i++;
+  if (i === 10) i = 0;
+
+  setTimeout(drawText, 1000);
+})();
+eu.handleTermination();
+
+eu.waitForInput(function() {
+  process.exit(0);
+});
