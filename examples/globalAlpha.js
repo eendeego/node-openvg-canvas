38a70b2c13fa7da0a872121c58758edf4c1c5805
Luis Reis <luis.m.reis@gmail.com>
2013-01-02 12:27:22 +0000

    Review globalAlpha example. It wasn't working due to issue #2. Fixes #6.

--- examples/globalAlpha.js
+++ examples/globalAlpha.js
@@ -13,8 +13,7 @@ var ctx = canvas.getContext('2d');
 var eu = require('./util');
 var fs = require('fs');
 
-ctx.clearRect(0, 0, canvas.width, canvas.height); //lll
-ctx.scale(4, 4);
+ctx.clearRect(0, 0, canvas.width, canvas.height);
 
 ctx.fillStyle = '#FD0';
 ctx.fillRect(0, 0, 75, 75);
@@ -32,9 +31,12 @@ ctx.globalAlpha = 0.2;
 // Draw semi transparent circles
 for (var i = 0; i < 7; i++) {
   ctx.beginPath();
-  ctx.arc(75, 75, 10 + 10 * i, 0, Math.PI * 2, true);
+  ctx.arc(75, 75, 10 + 10 * i, 0, Math.PI * 2, false);
   ctx.fill();
 }
+
+eu.saveScreenshot(ctx, 0, 0, 150, 150,
+                  'examples/screenshots/globalAlpha.png');
 
 canvas.vgSwapBuffers();
 eu.handleTermination();
